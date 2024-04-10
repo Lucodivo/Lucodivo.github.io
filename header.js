ba86c4b00345d26bb3da8e5e828e7dc196e07d98
Connor Haskins <ConnorAHaskins@gmail.com>
2024-04-09 23:28:00 -0700

    projects + links + my_collage

--- header.js
+++ header.js
@@ -1,7 +1,7 @@
 document.write(`\
 <div class="topnav" id="myTopnav">\
   <a href="index.html" class="active">Connor A. Haskins</a>\
-  <a href="blog.html">Blog</a> <a href="index.html">About</a>\
+  <a href="blog.html">Blog</a><a href="index.html">About</a><a href="projects.html">Projects</a>\
   <a href="javascript:void(0);" class="icon" onclick="myFunction()">\
     <i class="fa fa-bars"></i>\
   </a>\
